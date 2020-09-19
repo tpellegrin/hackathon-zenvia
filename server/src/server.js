250333c55d2be2f8a0cce9f5bf166f77e8453b75
tpellegrin <tgpellegrin@gmail.com>
2020-09-19 20:30:50 -0300

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -31,4 +31,9 @@ app.get('/', (request, response) => {
         .catch((error) => console.log('Error:', error));
 });
 
-app.listen(5555, () => console.log('Server initialized.'));
+const server = app.listen(process.env.PORT || 8080, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log(`App listening at http://${host}:${port}`);
+});
