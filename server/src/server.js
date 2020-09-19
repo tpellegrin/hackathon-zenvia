d5f1b6a0a8cc385531fa62ce6750be795c0f2ed1
tpellegrin <tgpellegrin@gmail.com>
2020-09-19 16:39:07 -0300

    Add post request to ZenAPI for testing

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const { json } = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello World');
+    let url = 'https://api.zenvia.com/v1/channels/whatsapp/messages';
+    let token = 'kXOfRzga7UHSbkneZiLqt38WEpbzrBClqha-';
+
+    fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-API-TOKEN': token
+            },
+            body: JSON.stringify({
+                'from': 'vagabond-wildcat',
+                'to': '5511953681182',
+                'contents': [{
+                    'type': 'text',
+                    'text': 'Teste servidor 2'
+                }],
+            })
+        })
+        .then(response => {
+            response.ok ?
+                    response.json().then(json => { console.log(json) }) : console.log(response);
+        })
+        .then(() => response.send("Mensagem enviada com sucesso."))
+        .catch((error) => console.log('Error:', error));
 });
 
 app.listen(5555, () => console.log('Server initialized.'));
